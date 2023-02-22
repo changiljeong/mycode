f335dffb034b725733592a09cee14b692e4d2f4e
changiljeong <changil.jeong@tlglearning.com>
2023-02-22 21:49:25 +0000

    uptil try catch

---
+++ iftest/condition01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+hostname = "MTG"
+
+if hostname == "MTG":
+    print("this hostname was found to be MTG")
---
+++ iftest2/ipask.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+
+ipchk = input("Apply an IP address: ") # this line now prompts the user for input
+
+
+if ipchk:
+   print("Looks like the IP address was set: " + ipchk) # indented under if
+
