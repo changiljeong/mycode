e736c80d53a3d5093b6369be7d4373873fc2404f
changiljeong <changil.jeong@tlglearning.com>
2023-02-27 18:04:50 +0000

    Python and JSON practice, modeling with lists(arrays) and dicts(ojjects)

--- oop2/cheatdice.py
+++ oop2/cheatdice.py
@@ -26,5 +26,27 @@ class Cheat_Loaded_Dice(Player):
             if self.dice[i] < 6:
                 self.dice[i] += 1
             i += 1
+
+class Cheat_Mulligan(Player):
+    def cheat(self):
+        if sum(self.dice) <= 9:
+            self.dice = []
+            for i in range(3):
+                self.dice.append(random.randint(1,6))
+
+class Cheat_Extra_Die(Player):
+    def cheat(self):
+        self.dice.append(random.randit(1,6))
+
+
+class Cheat_Lucky_Die(Player):
+    def cheat(self):
+        if self.dice[0] < 3:
+            sef.dice[0] = random.randint(3,6)
+
+class Cheat_Saboteur(Player):
+    def cheat(self, other_player):
+        other_player.dice = [random.randit(1,3) for i in range(3)]
+
 if __name__ == "__main__":
     main()
